955e6169827044c1e14fb0640894fb14a4e1ea1e
Ben Woosley <ben.woosley@gmail.com>
2008-04-22 00:52:13 -0700

    create a helper for properly renaming args

--- lib/amazon-a2s/requests/cart.rb
+++ lib/amazon-a2s/requests/cart.rb
@@ -1,5 +1,13 @@
 require File.join(File.dirname(__FILE__), '../request')
 
+class Hash
+  def map_keys!(keys)
+    keys.each_pair do |new, old|
+      store(new, delete(old)) if has_key?(old)
+    end
+  end
+end
+
 module Amazon
   class A2s
   private
@@ -26,7 +34,7 @@ module Amazon
     request :cart_add => :cart_id do |opts|
       opts["Item.#{asin}.Quantity"] = opts[:quantity] || 1
       opts["Item.#{asin}.ASIN"] = opts[:asin]
-      opts[:hMAC] = opts.delete(:hmac)
+      opts.map_keys!(:hMAC => :hmac)
     end
 
     # Adds item to remote shopping cart
@@ -34,9 +42,8 @@ module Amazon
       if opts.has_key? :id and opts.has_key? :cart_id and opts[:id] != opts[:cart_id]
         raise ArgumentError, "the id and cart_id parameters are both specified, when the have the same meaning"
       end
-      opts[:cart_id] = opts.delete(:id) if opts.has_key? :id
-      opts[:hMAC] = opts.delete(:hmac) if opts.has_key? :hmac
-      opts
+      opts.map_keys!(:cart_id => :id,
+                     :hMAC => :hmac)
     end
 
     # modifies _cart_item_id_ in remote shopping cart
@@ -47,12 +54,12 @@ module Amazon
       opts["Item.#{asin}.Quantity"] = opts.delete(:quantity)
       opts["Item.#{asin}.ASIN"] = asin
 
-      opts[:hMAC] = opts.delete(:hmac)
+      opts.map_keys!(:hMAC => :hmac)
     end
 
     # clears contents of remote shopping cart
     request :cart_clear => :cart_id do
-      opts[:hMAC] = opts.delete(:hmac)
+      opts.map_keys!(:hMAC => :hmac)
     end
   end
 end
