47ca54adce767752a52a776cc37644d45fa5ce6b
Ben Woosley <ben.woosley@gmail.com>
2008-05-02 01:22:54 -0700

    Refactor/simplify cart tests

--- test/amazon/cart_test.rb
+++ test/amazon/cart_test.rb
@@ -1,15 +1,18 @@
 require File.join(File.dirname(__FILE__), '../test_helper')
 
 class Amazon::A2s::CartTest < Test::Unit::TestCase
+  def setup
+    @potter = Amazon::A2s.item_search("potter").items[0]
+    @create_response = Amazon::A2s.cart_create(:items => {@potter.to_hash => 1})
+  end
+
   def test_cart_create
-    item = Amazon::A2s.item_search("potter").items[0].to_hash
-    assert !Amazon::A2s.cart_create(:items => {item => 1}).cart.blank?
+    assert !@create_response.cart.blank?
   end
 
   def test_cart_get
-    item = Amazon::A2s.item_search("potter").items[0].to_hash
-    cart = Amazon::A2s.cart_create(:items => {item => 1})
-    fetchedCart = Amazon::A2s.cart_get(cart.text_at(:cartid), :hmac => cart.text_at(:urlencodedhmac))
-    assert_equal cart.cart.items.to_s, fetchedCart.cart.items.to_s
+    fetchedCart = Amazon::A2s.cart_get(@create_response.text_at(:cartid),
+                                       :hmac => @create_response.text_at(:urlencodedhmac))
+    assert_equal @create_response.cart.items.to_s, fetchedCart.cart.items.to_s
   end
 end
