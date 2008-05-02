2d31f55ae5b33989f11a7f7d1df9c473489abcce
Ben Woosley <ben.woosley@gmail.com>
2008-05-02 01:15:45 -0700

    Oops, update tests for previous commit

--- test/amazon/cart_test.rb
+++ test/amazon/cart_test.rb
@@ -2,12 +2,12 @@ require File.join(File.dirname(__FILE__), '../test_helper')
 
 class Amazon::A2s::CartTest < Test::Unit::TestCase
   def test_cart_create
-    item = Amazon::A2s.item_search("potter").items[0].to_hash[:asin]
+    item = Amazon::A2s.item_search("potter").items[0].to_hash
     assert !Amazon::A2s.cart_create(:items => {item => 1}).cart.blank?
   end
 
   def test_cart_get
-    item = Amazon::A2s.item_search("potter").items[0].to_hash[:asin]
+    item = Amazon::A2s.item_search("potter").items[0].to_hash
     cart = Amazon::A2s.cart_create(:items => {item => 1})
     fetchedCart = Amazon::A2s.cart_get(cart.text_at(:cartid), :hmac => cart.text_at(:urlencodedhmac))
     assert_equal cart.cart.items.to_s, fetchedCart.cart.items.to_s
