0e4dd94650515055baf93278f6ea9374682d36fa
Ben Woosley <ben.woosley@gmail.com>
2008-08-31 14:31:15 -0700

    Extract Image#to_measurement helper

--- lib/amazon-a2s/types/image.rb
+++ lib/amazon-a2s/types/image.rb
@@ -1,30 +1,33 @@
 module Amazon
   class Image
     attr_reader :url, :width, :height
-    
+
     def initialize(url, width, height)
       @url = url
-      width = Measurement.new(width, 'pixels') unless width.is_a? Measurement
-      @width = width
-      height = Measurement.new(height, 'pixels') unless height.is_a? Measurement
-      @height = height
+      @width = to_measurement(width)
+      @height = to_measurement(height)
     end
-    
+
     def ==(other)
       return nil unless other.is_a? Image
       @url == other.url and @width == other.width and @height == other.height
     end
-    
+
     def size
       unless height.units == 'pixels' and width.units == 'pixels'
         raise 'size not available for images not denominated in pixels'
       end
-      
+
       "#{width.value.round}x#{height.value.round}"
     end
-    
+
     def inspect
       sprintf("#<%s: %s,%sx%s>", self.class.to_s, self.url, self.width, self.height)
-    end    
+    end
+
+  private
+    def to_measurement(arg)
+      arg.is_a?(Measurement) ? arg : Measurement.new(arg, 'pixels')
+    end
   end
 end
