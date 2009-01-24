717a253a09d3282aea8bb75c3928d89b5deefd34
Ben Woosley <ben.woosley@gmail.com>
2009-01-23 20:37:54 -0500

    Move measurement tests to rspec, exposing a bug...

---
+++ spec/measurement_spec.rb
@@ -0,0 +1,42 @@
+require 'amazon-associates'
+
+describe "measurement object", :shared => true do
+  it "should take a quantity and units" do
+    m = measurement_with(:value => 11.3, :units => 'inches')
+    m.value.should == 11.3
+    m.units.should == 'inches'
+  end
+
+  it "should default units to 'pixels', as we use it mostly for images" do
+    m = measurement_with(:value => 22.19)
+    m.value.should == 22.19
+    m.units.should == 'pixels'
+  end
+
+  it "should translate 'hundredths-' units to standard quantities" do
+    m = measurement_with(:value => 1130, :units => 'hundredths-inches')
+    m.value.should == 11.3
+    m.units.should == 'inches'
+  end
+end
+
+describe Amazon::Associates::Measurement do
+  describe "#initialize" do
+    def measurement_with(opts)
+      Amazon::Associates::Measurement.new(*opts.values_at(:value, :units).compact)
+    end
+
+    it_should_behave_like "measurement object"
+  end
+
+  describe "#from_xml" do
+    def measurement_with(opts)
+      doc = ROXML::XML::Document.new
+      doc.root = ROXML::XML::Node.new_element('width', opts[:value])
+      doc.root['Units'] = opts[:units] unless opts[:units].blank?
+      Amazon::Associates::Measurement.from_xml(doc.root)
+    end
+
+    it_should_behave_like "measurement object"
+  end
+end
