bd3a79d445e6d1c087a3c95c6ed4ffb596a0a5da
Ben Woosley <ben.woosley@gmail.com>
2009-03-22 23:06:02 -0400

    Remove the AWS_ACCESS_KEY_ID stuff, which should be set up in ENV anyway

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,9 +7,3 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__) , '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__) , 'utilities'))
 require 'amazon-associates'
 require 'filesystem_test_helper'
-
-AWS_ACCESS_KEY_ID = '0PP7FTN6FM3BZGGXJWG2'
-raise "Please specify set your AWS_ACCESS_KEY_ID" if AWS_ACCESS_KEY_ID.empty?
-
-Amazon::Associates.options.merge!(
-  :aws_access_key_id => AWS_ACCESS_KEY_ID)
