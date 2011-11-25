8644719f1990c31a740c9ef4a48c800a288f561b
Ben Woosley <ben.woosley@gmail.com>
2011-11-25 01:00:09 -0600

    Provide associate tag and access key id in the specs so most of them work - down to 25 failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require File.join(File.dirname(__FILE__), "../lib/amazon-associates")
 
+Amazon::Associates.options.merge!(
+  associate_tag: ENV['AMAZON_ASSOCIATE_TAG'], aws_access_key_id: ENV['AMAZON_ACCESS_KEY_ID'])
+
 shared_examples_for "Amazon Associates response" do
   describe "request" do
     subject { @response.request }
